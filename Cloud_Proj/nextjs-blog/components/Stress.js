d6e754b9b0b1f54ea13bdbeba8a0e5d209fc2c77
Tony <g00372842@gmit.ie>
2022-04-05 11:23:04 +0100

    go on 4

--- Cloud_Proj/nextjs-blog/components/Stress.js
+++ Cloud_Proj/nextjs-blog/components/Stress.js
@@ -74,7 +74,7 @@ function Stress() {
   return (
 
       <div className="StressBox">
-        <img src="STR.png" alt="STR"></img>
+        <img src="STR.png" alt="STR" className="center"></img>
           <button class="GoodBtn" auto onClick={() => incrementGood()}>Good</button>
           <button class="FineBtn" auto onClick={() => incrementFine()}>Fine</button>
           <button class="MildBtn" auto onClick={() => incrementMild()}>Mild</button>
